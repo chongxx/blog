e0f78b9f2579a4ae968f489f6ebbac402d589078
chongxx <choongzhaoo@foxmail.com>
2017-03-16 20:57:03 +0800

    chore: create user

--- config.py
+++ config.py
@@ -6,3 +6,7 @@ class ProdConfig(Config):
 
 class DevConfig(Config):
 	DEBUG= True
+	# config database uri
+	SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://root:123456@localhost:3306/blog'
+	# show sqlalchemy operate database log
+	SQLALCHEMY_ECHO = True
--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from flask import Flask
 from config import DevConfig
+from flask.ext.sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config.from_object(DevConfig)
+db = SQLAlchemy(app)
 
 @app.route('/')
 def home():
@@ -10,3 +12,19 @@ def home():
 
 if __name__ == '__main__':
 	app.run()
+
+
+# create a model
+class User(db.Model):
+
+	__tablename__ = 'user'
+
+	id = db.Column(db.Integer(), primary_key = True)
+	username = db.Column(db.String(255))
+	password = db.Column(db.String(255))
+
+	def __init__(self, username):
+		self.username = username
+
+	def __repr__(self):
+		return "User '{}'".format(self.username)
